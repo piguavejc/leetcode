c7abb736114f41deecd02ffbdb769d1cd85d7007
Jean carlos <piguavejc@gmail.com>
2024-06-16 04:51:59 +0000

    chore: Add binary search implementation

---
+++ src/binary-search-02/index.ts
@@ -0,0 +1,26 @@
+function searchInsert(nums: number[], target: number): number {
+  let left = 0;
+  let rigth = nums.length - 1;
+
+  if (target < nums[0]) {
+    return left;
+  }
+
+  while (left <= rigth) {
+    const middle = left + Math.floor((rigth - left) / 2);
+    const value = nums[middle];
+
+    if (target === value) {
+      return middle;
+    } else {
+      if (target > value && target < nums[middle + 1]) {
+        return middle + 1;
+      }
+    }
+
+    if (target > value) left = middle + 1;
+    else rigth = middle - 1;
+  }
+
+  return nums.length;
+}
